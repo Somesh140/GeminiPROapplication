fb8fc1adb7c1c59e4f4e798fe0991c78d499ebc7
Somesh140 <somesh140@gmail.com>
2024-01-21 20:42:35 +0530

    prompt update

--- food_calorie_app.py
+++ food_calorie_app.py
@@ -46,3 +46,18 @@ if uploaded_file is not None:
 
 
 submit=st.button("Tell me the total calories")
+
+input_prompt="""
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+
+
+"""
+
+## If submit button is clicked

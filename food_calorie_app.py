a920cded3eff2dd1fa5fe6c58289b6f04744d223
Somesh140 <somesh140@gmail.com>
2024-01-24 17:07:57 +0530

    update

--- food_calorie_app.py
+++ food_calorie_app.py
@@ -61,3 +61,8 @@ You are an expert in nutritionist where you need to see the food items from the 
 """
 
 ## If submit button is clicked
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
